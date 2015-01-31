6d840e97373cd2207a35c1eaa03f11ef0e4c3bba
Christopher Hewett <chewett@hotmail.co.uk>
2015-01-31 11:28:07 +0000

    Added more diagrams

--- move.py
+++ move.py
@@ -3,13 +3,17 @@ import time
 LEFT_CORRECTION = 1
 RIGHT_CORRECTION = 1
 
+DEGREES_PER_SECOND = 90
+DISTANCE_PER_SECOND = 1 #meters
+
+TURNING_POWER = 30
 
 '''
 ASCII art of the diagram
 
    0 m[1].m0
-  / \
- /   \
+  z z
+ z   z
 0-----0 m[0].m1
 
 '''
@@ -20,6 +24,23 @@ def forward(R, time):
     R.motors[0].m1.power = 100 * RIGHT_CORRECTION
     time.sleep(time)
 
-def distance_to_time(distance):
+    stop_all_motors(R)
 
-    return distance
+def stop_all_motors(R):
+    R.motors[0].m0.power = 0
+    R.motors[0].m1.power = 0
+    R.motors[1].m0.power = 0
+
+def left(R, degrees):
+    R.motors[0].m0.power = TURNING_POWER
+    R.motors[0].m1.power = TURNING_POWER
+    R.motors[1].m0.power = TURNING_POWER
+    time.sleep(abs(degrees / DEGREES_PER_SECOND))
+
+    stop_all_motors(R)
+
+def right(R, degrees):
+    left(R, -degrees)
+
+def distance_to_time(distance):
+    return (distance / DISTANCE_PER_SECOND)
