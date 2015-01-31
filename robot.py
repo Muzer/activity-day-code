a01c9284cedd5e60ac29a347edede27a9f675afc
Christopher Baines <mail@cbaines.net>
2015-01-31 11:34:34 +0000

    Some things

--- robot.py
+++ robot.py
@@ -8,10 +8,34 @@ import time
 match_length = 2 * 60
 start_time = time.clock()
 
-R = Robot()
-move.forward(R, 2)
-
 def match_running():
     match_time = time.clock() - start_time
 
     return match_time < match_length
+
+R = Robot()
+
+center_marker = 32
+
+if R.zone == 0: # Zone A
+    close_marker = 33
+    far_marker = 34
+else: # Zone B
+    close_marker = 34
+    far_marker = 33
+
+while match_running():
+    markers = R.see()
+
+    can_see_marker = False
+
+    for marker in markers:
+        if marker.info.code == far_marker: # The marker in the opposite zone
+            # move towards marker
+            move.forward(R, 2)
+
+            can_see_marker = True
+
+
+    if not can_see_marker:
+        print("Cannot see")
