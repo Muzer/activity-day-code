3e628ff9c2441d4b567b47fd5928f0245ec20623
Christopher Hewett <chewett@hotmail.co.uk>
2015-01-31 11:17:58 +0000

    Added ASCII art and correction factor

--- move.py
+++ move.py
@@ -1,9 +1,23 @@
 import time
 
+LEFT_CORRECTION = 1
+RIGHT_CORRECTION = 1
+
+
+'''
+ASCII art of the diagram
+
+   0 m[1].m0
+  / \
+ /   \
+0-----0 m[0].m1
+
+'''
+
 
 def forward(R, time):
-    R.motors[0].m0.power = 100
-    R.motors[0].m1.power = 100
+    R.motors[0].m0.power = 100 * LEFT_CORRECTION
+    R.motors[0].m1.power = 100 * RIGHT_CORRECTION
     time.sleep(time)
 
 def distance_to_time(distance):
