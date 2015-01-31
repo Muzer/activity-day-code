7ab417934db90a9a042bdca38e971469bf385eb9
Christopher Hewett <chewett@hotmail.co.uk>
2015-01-31 10:57:20 +0000

    Added moving forward code

---
+++ move.py
@@ -0,0 +1,4 @@
+
+def move_forward(R):
+    R.motors[0].m0.power = 100
+    R.motors[0].m1.power = 100
--- robot.py
+++ robot.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python2
 
 from sr import *
+import move
+
 
 R = Robot()
+move_forward(R)
 
-R.motors[0].m0.power = 100
-R.motors[0].m1.power = 100
+
