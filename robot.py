55f5233f217c966163c0aaf9d80385615bc09a11
Christopher Baines <mail@cbaines.net>
2015-01-31 11:05:39 +0000

    Add some time stuff

--- robot.py
+++ robot.py
@@ -3,8 +3,15 @@
 from sr import *
 import move
 
+import time
+
+match_length = 2 * 60
+start_time = time.clock()
 
 R = Robot()
 move.forward(R, 2)
 
+def match_running():
+    match_time = time.clock() - start_time
 
+    return match_time < match_length
